73176f4ae97e4a8761e70c1686510bd62a08f122
Ted K <ted.k07@gmail.com>
2023-02-11 23:01:03 +0100

    Add project files.

---
+++ SwitchExpressionZadanie/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SwitchExpressionZadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPodaj pierwszą liczbę ...");
+            double numberFirst = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę ...");
+            double numberSecend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPodaj numer operacji do wykonania:");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie\n");
+
+            int selected = int.Parse(Console.ReadLine());
+
+            var reaction = selected switch
+            {
+                1 => (numberFirst + numberSecend),
+                2 => (numberFirst - numberSecend),
+                3 => (numberFirst * numberSecend),
+                4 => (numberFirst / numberSecend),
+                _ => 0,
+            };
+            Console.WriteLine($"Twój wynik to {reaction}");
+
+        }
+    }
+}
